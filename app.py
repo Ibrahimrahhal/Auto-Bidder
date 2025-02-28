dd3a30f52ade51e3cbc1f0ae434a2b1cf0b55363
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2025-02-28 15:46:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,6 @@ def find_selected_elm(name, browser):
         if len(name_elm) > 0 and name in name_elm[0].text:
             return element
 
-
-
 def find_last_bid(auction_elm): 
     last_bid_elm = auction_elm.find_element(By.CSS_SELECTOR, ".auctioneers .body .row:first-child")
     last_bid_user_name = last_bid_elm.find_element(By.CLASS_NAME, "fb-user_name").text
